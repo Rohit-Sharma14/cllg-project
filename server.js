183880df5f047699500a3159199705596f7651c3
Rohit-sharma14 <14rohit09sharma99@gmail.com>
2020-10-20 10:57:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+
+
+const express = require('express')
+const app = express()
+const expresslayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expresslayouts)
+app.use(express.static('public'))
+
+
+//---------------connection to db----------------------------//
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('conneted to mongoose'))
+
+
+//-------------- use routes----------------------------------//
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
